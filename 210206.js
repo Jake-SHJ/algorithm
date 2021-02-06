0b9fc2aa8485c56aa562e2b6f3f1556be76273c6
jake <guswns1853@gmail.com>
2021-02-06 20:36:17 +0900

    210206 알고리즘 연습

---
+++ 210206.js
@@ -0,0 +1,28 @@
+// Richest Customer Wealth
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank.
+// Return the wealth that the richest customer has.
+
+// A customer's wealth is the amount of money they have in all their bank accounts.
+// The richest customer is the customer that has the maximum wealth.
+
+// Example:
+// Input: accounts = [[1,5],[7,3],[3,5]]
+// Output: 10
+// Explanation:
+// 1st customer has wealth = 6
+// 2nd customer has wealth = 10
+// 3rd customer has wealth = 8
+// The 2nd customer is the richest with a wealth of 10.
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+const maximumWealth = function (accounts) {
+  const sumOfAccountArr = accounts.map((account) => {
+    return account.reduce((acc, cur) => acc + cur);
+  });
+
+  return Math.max.apply(null, sumOfAccountArr);
+};
