20d1ea7a7b7926178658dcaaa18bef8466f4cf1c
jake <guswns1853@gmail.com>
2022-06-06 09:52:08 +0900

    feat: 220606 algorithm practice

---
+++ 2022/2206/220606.js
@@ -0,0 +1,39 @@
+// Flip Equivalent Binary Trees
+
+// For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+// A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+// Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
+
+// Example:
+// Flipped Trees Diagram
+// Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+// Output: true
+// Explanation: We flipped at nodes with values 1, 3, and 5.
+
+// 주어진 이진트리 2개가 서로 뒤집어진 경우인지 판단하는 문제
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var flipEquiv = function (root1, root2) {
+  if (root1 === root2) return true;
+
+  if (root1 === null || root2 === null || root1.val !== root2.val) return false;
+
+  return (
+    (flipEquiv(root1.left, root2.left) &&
+      flipEquiv(root1.right, root2.right)) ||
+    (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+  );
+};
+// solution 참조, 뒤집기를 할수도, 안할수도 있는 상황이라 문제 해결의 방향이 잘 잡히지 않음
